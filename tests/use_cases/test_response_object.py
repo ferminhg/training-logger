5f60bebf7b969de5655116cab70d719bab191f3c
fermin <fermin.hdez@gmail.com>
2019-05-22 16:44:54 +0200

    add test response

--- tests/use_cases/test_response_object.py
+++ tests/use_cases/test_response_object.py
@@ -1,5 +1,65 @@
-from trainings.shared import response_object as ro
+import pytest
 
-def test_response_success_is_true():
-    assert bool(ro.ResponseSuccess()) is True
+from trainings.shared import response_object as res
+from trainings.use_cases import request_objects as req
 
+
+@pytest.fixture
+def response_value():
+    return {'key': ['value1', 'value2']}
+
+
+@pytest.fixture
+def response_type():
+    return 'RepsonseError'
+
+
+@pytest.fixture
+def response_message():
+    return 'This is a response error'
+
+
+def test_response_success_is_true(response_value):
+    assert bool(res.ResponseSuccess(response_value)) is True
+
+
+def test_response_failure_is_false(response_type, response_message):
+    assert bool(res.ResponseFailure(response_type, response_message)) is False
+
+
+def test_response_success_contains_value(response_value):
+    response = res.ResponseSuccess(response_value)
+
+    assert response.value == response_value
+
+
+def test_response_failure_has_type_and_message(response_type, reponse_message):
+    response = res.ResponseFailure(response_type, response_message)
+
+    assert response.type == response_type
+    assert response.message == response_message
+
+
+def test_response_failure_contains_value(response_type, response_message):
+    response = res.ResponseSuccess(response_type, response_message)
+
+    assert response.value == {'type': response_type, 'message': response_message}
+
+
+def test_response_failure_initialization_with_exception():
+    response = res.ResponseFailure(response_type, Exception('Just an error message'))
+    
+    assert bool(response) is False
+    assert response.type == response_type
+    assert response.message == "Exception: Just an error message"
+
+
+def test_response_failure_from_invalid_request_objet():
+    response = res.ResponseFailure.build_from_invalid_request_object(req.InvalidRequestObject())
+
+    assert bool(response) is False
+
+
+def test_response_failure_from_invalid_request_object_with_errors():
+    request_object = req.InvalidRequestObject()
+    request_object.add_error('path')
